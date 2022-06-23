e3efc26dfd14ea59b972252b843ebd20b7b9a422
nathzwo <107952061+nathzwo@users.noreply.github.com>
2022-06-23 02:53:42 -0300

    acomodo

--- js/wpp.js
+++ js/wpp.js
@@ -1,25 +1,28 @@
-document.getElementById('whats-chat').addEventListener("mouseover", showchatbox);
-document.getElementById('chat-top-right').addEventListener("click", closechatbox);
-document.getElementById('send-btn').addEventListener("click", sendmsg);
+document
+  .getElementById("whats-chat")
+  .addEventListener("mouseover", showchatbox);
+document
+  .getElementById("chat-top-right")
+  .addEventListener("click", closechatbox);
+document.getElementById("send-btn").addEventListener("click", sendmsg);
 window.addEventListener("load", showchatboxtime);
 function showchatbox() {
-    document.getElementById('chat-box').style.right = '8%'
+  document.getElementById("chat-box").style.right = "8%";
 }
 function closechatbox() {
-    document.getElementById('chat-box').style.right = '-500px'
-
-
+  document.getElementById("chat-box").style.right = "-500px";
 }
 function showchatboxtime() {
-    setTimeout(launchbox, 5000)
+  setTimeout(launchbox, 5000);
 }
 function launchbox() {
-    document.getElementById('chat-box').style.right = '8%'
-
+  document.getElementById("chat-box").style.right = "8%";
 }
 function sendmsg() {
-    var msg = document.getElementById('whats-in').value;
-    var relmsg = msg.replace(/ /g, "%20");
-    window.open('https://api.whatsapp.com/send?phone=2614601746&text=' + relmsg, '_blank');
-
+  var msg = document.getElementById("whats-in").value;
+  var relmsg = msg.replace(/ /g, "%20");
+  window.open(
+    "https://api.whatsapp.com/send?phone=2614601746&text=" + relmsg,
+    "_blank"
+  );
 }
